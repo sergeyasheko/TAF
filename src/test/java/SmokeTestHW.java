a37886a862d45c40aefdfdbe27b6b9a8075d21b7
sergeyasheko <sergeypeh88@gmail.com>
2022-04-19 13:51:05 +0300

    Первая задача из домашки

---
+++ src/test/java/SmokeTestHW.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SmokeTestHW {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        BrowserService browserService = new BrowserService();
+        driver = browserService.getDriver();
+    }
+    @Test
+    public void electricUnderfloorHeatingCalculator() throws InterruptedException{
+        driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
+        WebElement width = driver.findElement(By.id("el_f_width"));
+        WebElement length = driver.findElement(By.id("el_f_lenght"));
+        WebElement heatLoss = driver.findElement(By.id("el_f_losses"));
+        WebElement button = driver.findElement(By.name("button"));
+
+        WebElement selectWebElement1 = driver.findElement(By.id("room_type"));
+        Select selectRoom = new Select(selectWebElement1);
+        WebElement selectWebElement2 = driver.findElement(By.id("heating_type"));
+        Select selectTypeOfHeating = new Select(selectWebElement2);
+
+        width.sendKeys("4");
+        Thread.sleep(1000);
+        length.sendKeys("5");
+        Thread.sleep(1000);
+        selectRoom.selectByValue("2");
+        Thread.sleep(1000);
+        selectTypeOfHeating.selectByIndex(2);
+        Thread.sleep(1000);
+        heatLoss.sendKeys("1200");
+        Thread.sleep(1000);
+        button.click();
+        Thread.sleep(1500);
+
+        WebElement heatingCableOrMatPower = driver.findElement(By.id("floor_cable_power"));
+        Assert.assertEquals(668,668);
+        WebElement specificPowerOfHeatingCableOrMat = driver.findElement(By.id("spec_floor_cable_power"));
+        Assert.assertEquals(33,33);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
