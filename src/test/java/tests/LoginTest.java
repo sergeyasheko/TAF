ba8b0a0ffd6fe763a9d5a203e8fe27b9f72307ec
sergeyasheko <sergeypeh88@gmail.com>
2022-05-25 19:55:13 +0300

    q

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -18,7 +18,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void incorrectEmailLoginTest() {
         Assert.assertEquals(
-                loginStep.incorrectLogin("sdsd", ReadProperties.password()).getErrorTextElement().getText(),
+                loginStep.incorrectLogin("sdsd", ReadProperties.password()).errorText.getText(),
                 "Email/Login or Password is incorrect. Please try again.",
                 "Неверное сообщение об ошибке");
         Assert.assertTrue(false);
@@ -27,7 +27,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void incorrectPswLoginTest() {
         Assert.assertEquals(
-                loginStep.incorrectLogin(ReadProperties.username(), "123").getErrorTextElement().getText(),
+                loginStep.incorrectLogin(ReadProperties.username(), "123").errorText.getText(),
                 "Email/Login or Password is incorrect. Please try again.",
                 "Неверное сообщение об ошибке");
     }
