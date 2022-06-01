3af2096e0c833dfc188a7787233eea35e600b4f6
sergeyasheko <sergeypeh88@gmail.com>
2022-06-01 16:34:17 +0300

    q

--- src/main/java/services/WaitsService.java
+++ src/main/java/services/WaitsService.java
@@ -1,15 +1,19 @@
 package services;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class WaitsService {
     private WebDriverWait wait;
+    private WebDriver driver;
 
     public WaitsService(WebDriver driver, Duration timeout) {
         wait = new WebDriverWait(driver, timeout);
@@ -25,5 +29,19 @@ public class WaitsService {
 
     public WebElement waitForExists(By locator) {
         return wait.until(ExpectedConditions.presenceOfElementLocated(locator)); // ожидалка, ищем элемент в дом модели по локатору, и если он есть возвращаем
+    }
+
+    public Boolean waitForElementInvisible(WebElement element) {
+        return wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public WebElement fluentWaitForElement(By locator) {
+        Wait<WebDriver> fluent = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(50))
+                .ignoring(NoSuchElementException.class);
+
+        return fluent.until(driver1 -> driver.findElement(locator));
+
     }
 }
