7e0560049a5af35297e275ffd804b4376bead3e5
sergeyasheko <sergeypeh88@gmail.com>
2022-06-06 11:19:13 +0300

    q

--- src/test/java/tests/DynamicControlsTest.java
+++ src/test/java/tests/DynamicControlsTest.java
@@ -24,9 +24,30 @@ public class DynamicControlsTest extends BaseTest {
         actions.moveToElement(buttonRemove).click().build().perform();
 
         WebElement title = wait.waitForVisibilityLocatedBy(By.xpath("//p[@id='message']"));
-        actions.moveToElement(title).build().perform();
+        Assert.assertEquals(title.getText(),"It's gone!");
 
         Assert.assertTrue(wait.waitForElementInvisible(checkBox));
 
+    }
+
+    @Test
+    public void inputTest() throws InterruptedException {
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+
+        WaitsService wait = new WaitsService(driver, Duration.ofSeconds(10));
+
+        WebElement inputField = wait.waitForExists(By.xpath("//input[@type='text']"));
+        Assert.assertFalse(inputField.isEnabled());
+
+        WebElement buttonSwapInput = wait.waitForExists(By.xpath("//button[@onclick='swapInput()']"));
+        actions.moveToElement(buttonSwapInput).click().build().perform();
+
+        WebElement title = wait.waitForVisibilityLocatedBy(By.xpath("//p[@id='message']"));
+        Assert.assertEquals(title.getText(),"It's enabled!");
+
+        actions.moveToElement(inputField).click().build().perform();
+        Assert.assertTrue(inputField.isEnabled());
+
+        Thread.sleep(2000);
     }
 }
