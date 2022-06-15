c06f9973698eec5d4117a8e278cc26c9e35c7043
sergeyasheko <sergeypeh88@gmail.com>
2022-06-15 22:07:15 +0300

    R

---
+++ src/test/java/tests/db/BaseDBTest.java
@@ -0,0 +1,26 @@
+package tests.db;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import services.DataBaseService;
+
+public class BaseDBTest {
+    Logger logger = LoggerFactory.getLogger(BaseDBTest.class);
+
+    public DataBaseService dataBaseService;
+
+    public BaseDBTest() {
+    }
+
+    @BeforeTest
+    public void setupConnection() {
+        dataBaseService = new DataBaseService();
+    }
+
+    @AfterTest
+    public void closeConnection() {
+        dataBaseService.closeConnection();
+    }
+}
