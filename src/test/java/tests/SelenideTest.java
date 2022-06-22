1600d601d174c560d50c6ea361bd4ba9b540785c
sergeyasheko <sergeypeh88@gmail.com>
2022-06-22 13:34:33 +0300

    R

--- src/test/java/tests/SelenideTest.java
+++ src/test/java/tests/SelenideTest.java
@@ -1,22 +1,17 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import configuration.ReadProperties;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
-import java.io.File;
-
-import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideTest {
 
@@ -40,18 +35,7 @@ public class SelenideTest {
         // );
     }
 
-    @Test
-    public void simpleSelenideTest() {
-        open(ReadProperties.getUrl());
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
-        $(".page_title").shouldBe(visible).shouldHave(text("All Projects1"));
-    }
-
-    @Test
+       @Test
     public void poSelenideTest() {
         open(ReadProperties.getUrl());
 
@@ -61,103 +45,7 @@ public class SelenideTest {
         loginPage.password.setValue(ReadProperties.password());
         loginPage.loginButton.click();
 
-        $(".page_title").shouldBe(visible).shouldHave(text("All Projects1"));
-    }
-
-    @Test
-    public void selenideTest1() {
-        open("/");
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
         $(".page_title").shouldBe(visible).shouldHave(text("All Projects"));
-        $$("div.project")
-                .filter(visible)
-                .shouldHave(size(26))
-                .find(text("erg"))
-                .click();
-        $$("div.project")
-                .first();
-        $$("div.project")
-                .get(2);
-
-        sleep(1000);
-        refresh();
-        title();
-        executeJavaScript("");
     }
 
-    @Test
-    public void SelenideTest2() {
-        open(ReadProperties.getUrl());
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
-        System.out.println($(byText("asdasdasdfaszc")).innerText()); //текст который отображается на экране
-        System.out.println($(byText("asdasdasdfaszc")).getText());
-        System.out.println($(byText("asdasdasdfaszc")).innerHtml());
-        System.out.println($(byText("asdasdasdfaszc")).data(""));
-        System.out.println($(byText("asdasdasdfaszc")).val());
-        System.out.println($(byText("asdasdasdfaszc")).getValue()); // получить значение со строк инпута
-        $(byText("asdasdasdfaszc")).scrollTo();
-        $(byText("asdasdasdfaszc")).doubleClick();
-        $(byText("asdasdasdfaszc")).contextClick();
-        $(byText("asdasdasdfaszc")).hover();
-        $(byText("asdasdasdfaszc")).find(By.xpath(""));
-        $(byText("asdasdasdfaszc")).closest("tr");    // найти ближайший элемент
-        $(byText("asdasdasdfaszc")).ancestor("div");
-        $(byText("asdasdasdfaszc")).sibling(1);
-        $(byText("asdasdasdfaszc")).parent();
-
-        $(byText("asdasdasdfaszc")).uploadFile(new File(""));
-        // $(byText("asdasdasdfaszc")).download();
-    }
-
-    @Test
-    public void SelenideTest3() {
-        open("/");
-
-        $(By.id("name")).setValue(ReadProperties.username());
-        $("#password").setValue(ReadProperties.password());
-        $("#button_primary").click();
-
-        open("open('/index.php?/admin/projects/overview');");
-        $$(By.className("hoverSensitive"))
-                .shouldHave(size(26))
-                .find(text("erg"))
-                .find(By.tagName("a"))
-                .click();
-
-        $("#announcement")
-                .should(exist)
-                .shouldBe(visible)
-                .shouldHave(exactText("This is the description for the project1"));
-
-        $("#announcement")
-                .should(exist).should(hidden).should(disappear).shouldNotBe(visible)
-                .should(readonly)
-                .shouldHave(name("name"))
-                .shouldHave(value("John"))
-                .shouldHave(type("checkbox"))
-                .shouldBe(empty)
-                .shouldBe(focused);
-
-        WebElement webElement = $("#announcement").toWebElement();
-
-        Condition clickable = and("can be clicked", visible, enabled);
-        $$("#announcement").findBy(clickable).click();
-
-        $("#announcement").shouldBe(clickable);
-
-        $("#announcement")
-                .shouldHave(text("Expected Text"))   //проверка на ожидаемый текст, аналог contains
-                .shouldHave(matchText("Expected Text"))  // сверка с текстом с шаблона, который нужно прописать где-нибудь, regex
-                .shouldHave(exactText(""))  //проверка на текст полностью с пробелами и т.д., аналог equalsIgnoreCase
-                .shouldHave(textCaseSensitive("Expected Result Text")) // проверка текста с учетом регистра
-                .shouldHave(exactTextCaseSensitive("Expected Result Text"));  // проверка текста с учетом регистра и всех символов, аналог equals
-    }
 }
